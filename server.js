81717231920aa07b68b625cbec07fb2ac3d6e80c
andmondo111 <97679418+andmondo111@users.noreply.github.com>
2022-01-13 14:51:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
-  setHeaders: [{"Origin": "https://www.twitch.tv"}],
+  setHeaders: {"rigin": "https://www.twitch.tv"},
   removeHeaders: [
     'cookie',
     'cookie2',
